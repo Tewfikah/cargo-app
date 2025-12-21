98584a072ec652ab354b7714c78b01982b6fc30b
Tewfik Ahmed <tofik67890@gmail.com>
2025-12-20 23:20:01 -0800

    navbar link photo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-
+import Login from './pages/Login'
+import Register from './pages/Register'
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
       </Routes>
     </>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import image from '../assets/image.png'
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-6">
-        <div className="flex justify-between items-center h-16">
-
-          {/* Logo */}
-          <Link to="/" className="text-xl font-bold text-blue-600">
-            Smart Transport
+    <nav className='bg-white shadow-sm sticky top-0 z-50'>
+     <div className='max-w-7x1 mx-auto px-6'>
+      <div className='flex justify-between items-center h-16'>
+   {/* Logo */}
+          <Link to="/" className="flex items-center space-x-2">
+            <img
+              src={image}
+              alt="SmartCargo Logo"
+              className="h-15 w-15"
+            />
+            <span className="font-bold text-lg text-gray-800">
+              Smart<span className="text-blue-600">Cargo</span>
+            </span>
           </Link>
-          {/* Navigation Links */}
-          <div className='hidden md:flex items-center space-x-6'>
-       <Link to="/" className='text-gray-700 hover:text-blue-600'>
-          Home
-       </Link>
       </div>
-    </div>
-   </div>
+     </div>
     </nav>
   )
 }
